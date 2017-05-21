20d4162e90fccfe2d7edb7d53d834ed4e56d2cb3
Felipe Arruda Pontes <felipe.arruda.pontes@gmail.com>
2017-05-21 15:27:59 -0300

    creating ControlSystem when entering function block

--- scikit_fuzzy_fcl/fcl_listener.py
+++ scikit_fuzzy_fcl/fcl_listener.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
 from __future__ import absolute_import
 import sys
+from skfuzzy.control.controlsystem import ControlSystem
+
 from .fcl_parser import FclParserException
 
 if sys.version_info >= (3, 0):  # pragma: no cover
@@ -8,10 +10,13 @@ if sys.version_info >= (3, 0):  # pragma: no cover
 else:  # pragma: no cover
     from .py2_parser.FclListener import FclListener  # noqa: F403,F401
 
+# import numpy as np
+# from skfuzzy.control.antecedent_consequent import Antecedent  # , Consequent
+
 
 class ScikitFuzzyFclListener(FclListener):
     """
-    FclListener responsable of transforming the parsed Fcl file
+    FclListener responsable for transforming the parsed Fcl file
     into corresponding scikit-fuzzy objects
     """
     def __init__(self):
@@ -20,3 +25,6 @@ class ScikitFuzzyFclListener(FclListener):
 
     def visitErrorNode(self, node):
         raise FclParserException(node)
+
+    def enterFunction_block(self, ctx):
+        self.control_system = ControlSystem()
